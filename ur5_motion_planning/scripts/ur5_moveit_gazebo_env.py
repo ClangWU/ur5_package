e72f546b85afb111e6bfd2fae40d0719624815d1
clangwu <clangwu@163.com>
2023-11-03 20:08:43 +0800

    [add]moveit api for ur5

--- ur5_motion_planning/scripts/ur5_moveit_gazebo_env.py
+++ ur5_motion_planning/scripts/ur5_moveit_gazebo_env.py
@@ -9,30 +9,48 @@ import actionlib
 import control_msgs.msg
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
-def set_gripper(action_gripper, value):
-    # Initialize the goal as a FollowJointTrajectoryGoal
-    goal = control_msgs.msg.FollowJointTrajectoryGoal()
+# Workaround to use the gripper action client with unified gripper interface
+class GripperCommanderGroup:
 
-    # Create a trajectory point
-    trajectory_point = JointTrajectoryPoint()
-    trajectory_point.positions = [value]  # Set the gripper position
-    trajectory_point.time_from_start = rospy.Duration(3)  # Move to the position in 3 seconds
+    def __init__(self) -> None:    
 
-    # Create a trajectory and add the trajectory point to it
-    trajectory = JointTrajectory()
-    trajectory.joint_names = ["robotiq_85_left_knuckle_joint"]  # This should be the name of your gripper joint
-    trajectory.points.append(trajectory_point)
+        self.init_clients()
+        print("Gripper action clients ready")
 
-    # Set the trajectory in the goal
-    goal.trajectory = trajectory
 
-    # Send the goal
-    action_gripper.send_goal(goal)
-    # Wait for the server to finish performing the action
-    action_gripper.wait_for_result(rospy.Duration(10))
+    def init_clients(self):
+        self.action_gripper = actionlib.SimpleActionClient(
+        '/gripper_controller/follow_joint_trajectory',
+        control_msgs.msg.FollowJointTrajectoryAction
+        )
+        print("Waiting for action of gripper controller")
+        _ans = self.action_gripper.wait_for_server(rospy.Duration(5))
+        if _ans:
+            rospy.loginfo("Action server started")
+        elif not _ans:
+            rospy.loginfo("Action server not started") 
 
-    # Return the result of executing the action
-    return action_gripper.get_result()
+    def open_gripper(self, value=0.08):
+        self.set_gripper(value)
+
+    def close_gripper(self, value=0.0):
+        self.set_gripper(value)
+
+    def set_gripper(self, value):
+        goal = control_msgs.msg.FollowJointTrajectoryGoal()
+        # Create a trajectory point
+        trajectory_point = JointTrajectoryPoint()
+        trajectory_point.positions = [(0.08 - value)*10]  # Set the gripper position
+        trajectory_point.time_from_start = rospy.Duration(3)  # Move to the position in 3 seconds
+
+        trajectory = JointTrajectory()
+        trajectory.joint_names = ["robotiq_85_left_knuckle_joint"]  # This should be the name of your gripper joint
+        trajectory.points.append(trajectory_point)
+        goal.trajectory = trajectory
+
+        self.action_gripper.send_goal(goal)
+        self.action_gripper.wait_for_result(rospy.Duration(10))
+        return self.action_gripper.get_result()
 
 def main():
     moveit_commander.roscpp_initialize(sys.argv)
@@ -48,30 +66,23 @@ def main():
     move_group_gripper = moveit_commander.MoveGroupCommander(group_name_gripper)
     move_group_manipulator = moveit_commander.MoveGroupCommander(group_name_manipulator)
 
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory)
+
+
     rospy.loginfo("Planning frame: %s" % move_group_manipulator.get_planning_frame())
     rospy.loginfo("End effector link: %s" % move_group_manipulator.get_end_effector_link())
     rospy.loginfo("Robot Groups: %s" % robot.get_group_names())
     rospy.loginfo("Robot State:")
     rospy.loginfo(robot.get_current_state())
 
-    action_gripper = actionlib.SimpleActionClient(
-      '/gripper_controller/follow_joint_trajectory',
-      control_msgs.msg.FollowJointTrajectoryAction
-    )
-    print("Waiting for action of gripper controller")
-    _ans = action_gripper.wait_for_server(rospy.Duration(5))
-
-    if _ans:
-        rospy.loginfo("Action server started")
-    elif not _ans:
-        rospy.loginfo("Action server not started") 
+    GripperCommander = GripperCommanderGroup()
     # Planning to a Pose goal
     pose_goal = Pose()
     pose_goal.position.x = 0.0
-    pose_goal.position.y = 0.4
-    pose_goal.position.z = 0.4
-    pose_goal.orientation.w = 0
-    pose_goal.orientation.x = 1.0
+    pose_goal.position.y = 0.3
+    pose_goal.position.z = 0.6
+    pose_goal.orientation.w = 1.0
+    pose_goal.orientation.x = 0
     pose_goal.orientation.y = 0
     pose_goal.orientation.z = 0
 
@@ -87,19 +98,16 @@ def main():
     else:
         rospy.loginfo("Motion to pose goal failed.")
 
-
-    # Closing the gripper
+    # Moving the gripper
     rospy.loginfo("Closing the gripper")
-    set_gripper(action_gripper, 0.8)
-        # Opening the gripper
+    GripperCommander.close_gripper()
     rospy.loginfo("Opening the gripper")
-    set_gripper(action_gripper, 0.0)
-    # Closing the gripper
+    GripperCommander.open_gripper()
+    # Moving the gripper
     rospy.loginfo("Closing the gripper")
-    set_gripper(action_gripper, 0.8)
-        # Opening the gripper
+    GripperCommander.close_gripper()
     rospy.loginfo("Opening the gripper")
-    set_gripper(action_gripper, 0.0)
+    GripperCommander.open_gripper()
 
 if __name__ == '__main__':
     try:
